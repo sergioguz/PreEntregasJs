4dee3cc924aafa20162df5043ea5eb2812635779
sergioguz <sergiopianist@hotmail.com>
2024-05-21 23:05:24 -0400

    Pre Entrega 3

--- js/main.js
+++ js/main.js
@@ -1,115 +1,284 @@
-//Consulta de precios-PreEntrega2
+//Consulta de precios-PreEntrega3
 
-class Producto {
-    constructor(id,producto, precio,supermercado) {
-        this.id  = parseInt(id);
-        this.producto= producto;
-        this.precio  = parseFloat(precio);
-        this.supermercado= supermercado;
+let productos1=[
+
+    {"product" : "ACEITE DE SOYA CRISOL"},
+
+    {   
+        "type": 1,
+        "markets": [
+            {
+                "name": "Arroz",
+                "avg_price": 315,
+                "udmedida": "1 LIBRA",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 125 },
+                    { "name": "Ole", "price": 70 },
+                    { "name": "La Cadena", "price": 100 },
+                    { "name": "Fresh", "price": 170 }
+                ]
+                //"image": "https://3.bp.blogspot.com/-VWGxBMidZ1I/W6RUplh-7zI/AAAAAAAAnrs/giUUK2EDEQoBtFlogvfwkNa48oXtH0b3QCLcBGAs/s1600/aprezio.jpg"
+            },
+            {
+                "name": "Arroz",
+                "avg_price": 315,
+                "udmedida": "0.3 oz",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 100 },
+                    { "name": "Ole", "price": 75 },
+                    { "name": "La Cadena", "price": 110 },
+                    { "name": "Fresh", "price": 180 }
+                ]
+                //"image": "https://3.bp.blogspot.com/-VWGxBMidZ1I/W6RUplh-7zI/AAAAAAAAnrs/giUUK2EDEQoBtFlogvfwkNa48oXtH0b3QCLcBGAs/s1600/aprezio.jpg"
+            },
+            {
+                "name": "Ajo",
+                "avg_price": 319,
+                "udmedida": "1 LIBRA",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 55 },
+                    { "name": "Ole", "price": 75 },
+                    { "name": "La Cadena", "price": 80 },
+                    { "name": "Fresh", "price": 65 }]
+            },
+            {
+                "name": "Habichuelas",
+                "avg_price": 319.95,
+                "udmedida": "800 GRAMO",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 65 },
+                    { "name": "Ole", "price": 80 },
+                    { "name": "La Cadena", "price": 81 },
+                    { "name": "Fresh", "price": 77 }]
+            },
+            {
+                "name": "Pollo",
+                "avg_price": 319.95,
+                "udmedida": "1 LIBRA",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 65 },
+                    { "name": "Ole", "price": 80 },
+                    { "name": "La Cadena", "price": 81 },
+                    { "name": "Fresh", "price": 77 }]
+            },
+            {
+                "name": "Habichuelas Jiras",
+                "avg_price": 414,
+                "udmedida": "400 GRAMO",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 60 },
+                    { "name": "Ole", "price": 71 },
+                    { "name": "La Cadena", "price": 88 },
+                    { "name": "Fresh", "price": 77 }]
+            },
+            {
+                "name": "Aguacate",
+                "avg_price": 318,
+                "udmedida": "UNIDAD",
+                "supermarkets" : [
+                    { "name": "Bravo", "price": 60 },
+                    { "name": "Ole", "price": 71 },
+                    { "name": "La Cadena", "price": 88 },
+                    { "name": "Fresh", "price": 77 }]
+            },
+            {
+                "name": "Spaguettis",
+                "avg_price": 319.95,
+                "udmedida": "1 LIBRA",
+                "supermarkets": [
+                    { "name": "Bravo", "price": 120 },
+                    { "name": "Ole", "price": 130 },
+                    { "name": "La Cadena", "price": 125 },
+                    { "name": "Fresh", "price": 135 }
+                ]
+            },
+            {
+                "name": "Spaguettis Pre Cocidos",
+                "avg_price": 414,
+                "udmedida": "0.3 oz",
+                "supermarkets": [
+                    { "name": "Bravo", "price": 110 },
+                    { "name": "Ole", "price": 115 },
+                    { "name": "La Cadena", "price": 120 },
+                    { "name": "Fresh", "price": 125 }
+                ]
+            },
+            {
+                "name": "Aguacate Criollo",
+                "avg_price": 417,
+                "udmedida": "1 LIBRA",
+                "supermarkets": [
+                    { "name": "Bravo", "price": 140 },
+                    { "name": "Ole", "price": 145 },
+                    { "name": "La Cadena", "price": 150 },
+                    { "name": "Fresh", "price": 155 }
+                ]
+            },
+            {
+                "name": "Mani",
+                "avg_price": 379,
+                "udmedida": "1 oz",
+                "supermarkets": [
+                    { "name": "Bravo", "price": 100 },
+                    { "name": "Ole", "price": 105 },
+                    { "name": "La Cadena", "price": 110 },
+                    { "name": "Fresh", "price": 115 }
+                ]
+            },
+            {
+                "name": "Mani Pre cocido",
+                "avg_price": 318,
+                "udmedida": "1 LIBRA",
+                "supermarkets": [
+                    { "name": "Bravo", "price": 90 },
+                    { "name": "Ole", "price": 95 },
+                    { "name": "La Cadena", "price": 100 },
+                    { "name": "Fresh", "price": 105 }
+                ]
     }
-};
-
-const productos = [{ id: 1,  producto: "Arroz", precio: 125, supermercado: "Bravo" },
-                  {  id: 1,  producto: "Arroz", precio: 70, supermercado: "La Cadena" }, 
-                  {  id: 1,  producto: "Arroz"  , precio: 50, supermercado: "Fresh"},
-                  {  id: 1,  producto: "Arroz" , precio: 100, supermercado: "Ole"},
-                    { id: 13,  producto: "Ajo", precio: 109, supermercado: "Bravo" },
-                    { id: 13,  producto: "Ajo", precio: 109, supermercado: "Ole" }, 
-                    { id: 13,  producto: "Ajo", precio: 118, supermercado: "Unidos" },
-                    { id: 13,  producto: "Ajo", precio: 119.9, supermercado: "Jumbo" },
-                    { id: 10,  producto: "Habichuelas", precio: 50, supermercado: "Bravo" },
-                    { id: 10,  producto: "Habichuelas", precio: 30, supermercado: "Ole" }, 
-                    { id: 10,  producto: "Habichuelas", precio: 40, supermercado: "La Cadena"},
-                    { id: 10,  producto: "Habichuelas", precio: 55, supermercado: "Fresh" },
-                    { id: 8,  producto: "Pollo", precio: 65, supermercado: "Bravo" },
-                    { id: 8,  producto: "Pollo", precio: 55, supermercado: "Unidos" },
-                    { id: 8,  producto: "Pollo", precio: 63, supermercado: "Ole" }, 
-                    { id: 8,  producto: "Pollo", precio: 60, supermercado: "Jumbo" }];
+]}
+];
 
 
-let fechaUTC = new Date().toDateString();
 
-menuprincipal=true
+const container= document.getElementById('container');
 
+///Funcion para crear una Session Storage de la lista de resultado
+const guardarSession= (clave,valor) => sessionStorage.setItem(clave,JSON.stringify(valor));
 
-function Consultarprecios(){
+function crearCard(){
 
-    const opcionselect=parseInt(prompt("Bienvenido a Precios Bajo. ¿Que quisieras hacer? \n 1. Consultar el precio de un producto \n2. Consultar una lista de productos \n3. Agregar/Actualizar producto"))
+    productos1[1].markets.forEach((el,index) =>{
+        const card = document.createElement("div");
+        card.className= 'carta';
+         
+        const titulos= document.createElement("p");
+        const precios= document.createElement("p");
+        const button= document.createElement('button');
 
-
-    if (opcionselect===1){
-           
-            const consultaproductos= parseInt(prompt("Elige uno de los productos a continuacion para consulta \n 1.Arroz \n 8.Pollo \n 10.Habichuelas \n 13.Ajo "));
-            
-            ///filtro por el numero de opcion 
-            const resultadoconsulta= productos.filter((el)=> el.id=== consultaproductos);
+        const undmedidas = document.createElement('select');
+        undmedidas.id= 'undmedidas-' + el.name ;
     
-            ///Organicé de menor a mayor los precios
-            resultadoconsulta.sort((a, b) => a.precio - b.precio);
+        //Crear una option por defecto
+        const defaultOption= document.createElement('option');
+        defaultOption.value= '';
+        defaultOption.text= 'UdMedida';
+        defaultOption.disabled= true;
+        defaultOption.selected= true;
+        undmedidas.appendChild(defaultOption);
     
-            alert("Favor visualizar la consola para los ver los resultados");
-    
-            console.log('Aqui los precios por supermercados de '+ resultadoconsulta[0]["producto"], fechaUTC);
-    
-            for (const results of resultadoconsulta){
-                console.log(results["supermercado"],results["precio"]);
-            }
-    
-            }else if(opcionselect===2){
-    
-                let busquedalista= [];
-                let validaconsulta= true;
-    
-                while (validaconsulta){
-                    const consultaproductos= parseInt(prompt("Elige el codigo del producto a continuacion para consulta de la lista \n 1.Arroz \n 8.Pollo \n 10.Habichuelas \n 13.Ajo "));
-                    ///Agrega el producto 
-                    busquedalista.push(consultaproductos);
+
+        const option = document.createElement('option');
+        option.value = el.udmedida;
+        option.text = el.udmedida;
+        undmedidas.appendChild(option);
+
         
-                    
-                    let validasalir=prompt("Finalizar la consulta? Si o No?");
-                        if (validasalir. toLowerCase()=== 'no'){
-                            ///console.log(busquedalista);
-                        }else {
-                            ///filtro por el numero de opcion 
-                           
-                            const resultadoconsulta= productos.filter(el=>busquedalista.includes(el.id));
-                                    //Utilizo reduce para sumarizar
-                                const sumasPorSupermercado = resultadoconsulta.reduce((acum, producto) => {
-                                    // Si el supermercado ya está en el acumulador, sumar el precio actual al acumulado
-                                    if (acum[producto.supermercado]) {
-                                    acum[producto.supermercado] += producto.precio;
-                                    } else {
-                                    // Si no, inicializar el acumulador para ese supermercado con el precio del primer producto
-                                    acum[producto.supermercado] = producto.precio;
-                                    }
-                                    return acum;
-                                }, {});
-                                    //Me transforma el array a un objeto
-                                const paresSupermercadoPrecio = Object.entries(sumasPorSupermercado);
-                                    //Me organiza los de menor a mayor los precios
-                                paresSupermercadoPrecio.sort((a, b) => a[1] - b[1]);
-                                alert("Favor visualizar la consola para los ver los resultados");
-                                console.log('Aqui la lista de precios por supermercados al', fechaUTC);
-                                console.log(paresSupermercadoPrecio);
-                                
-                            break;
-                            
-                        }
-                }
+        // const imga1= document.createElement('img');
+        // imga1.src= el.image;
+        // imga1.alt= 'Imagen de '+ el.name;
+        // imga1.className= 'img'
     
-    
-            } else if (opcionselect===3){
-                productos.push(new Producto(prompt('Ingresa el ID que quieres que lleve'),
-                prompt('Ingresa el producto que quieres'),
-                prompt('Ingresa el precio'),
-                prompt('Ingresa el supermercado al que compraste')))
-                alert("Favor visualizar la consola para los ver los resultados");
-                console.log(productos);
-    
-            }else{
-                alert ("Esta opción no está disponible");     
-            }
+        titulos.innerHTML= el.name;
+        precios.innerHTML= el.avg_price;
+        button.innerHTML = 'AGREGAR';
+        button.id= 'agregar' + index;
+        button.addEventListener('click',()=>agregarAlResumen(el,undmedidas));
+        
 
+        
+         card.appendChild(titulos);
+         card.appendChild(precios);
+         card.appendChild(button);
+         card.appendChild(undmedidas);
+    
+         
+         //card.appendChild(imga1)
+        
+         container.appendChild(card)
+        });
+        
 };
 
-Consultarprecios();
+function agregarAlResumen(producto,selectElement){
+    const listaResumen= document.getElementById('listaResumen');
+    const selectedValue= selectElement.value;
+
+    
+
+    if(selectedValue){
+        const item= document.createElement('li');
+        item.innerText= `${producto.name} - ${selectedValue}`  
+        listaResumen.appendChild(item);
+    }else {
+        alert ('Por favor, seleccione una unidad de medida.');
+    }
+    
+};
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+function compararPrecios(){
+    const listaResumen= document.getElementById('listaResumen');
+    const items= listaResumen.getElementsByTagName('li');
+    const resumen= [];
+    
+    ///Leer el elemento html que contiene la opcion seleccionada por el cliente
+    for (const item of items) {
+        const [name, selectedValue] = item.innerText.split(' - ');
+        const product = productos1[1].markets.find(p => p.name === name);
+        if (product) {
+            resumen.push({ name: product.name, selectedValue: selectedValue, supermarkets: product.supermarkets });
+        }
+    };
+    
+    const supermercadoTotales= {};
+
+    ///Almacenar en el array supermercadoTotales la sumatoria de los totales seleccionados
+    resumen.forEach(item => {
+        item.supermarkets.forEach(supermarket => {
+            if (!supermercadoTotales[supermarket.name]) {
+            supermercadoTotales[supermarket.name]=0;
+        }
+            supermercadoTotales[supermarket.name]+=supermarket.price;
+
+        });
+    });
+    //Crear un objeto del nombre del supermercado y total
+    const supermercadoArray= Object.keys(supermercadoTotales).map(name=>({
+        name,
+        total: supermercadoTotales[name]
+    }));
+    
+    //Organizar de menor a mayor el total de precio para mostrar el mas barato primero
+    supermercadoArray.sort((a,b)=> a.total - b.total);
+
+    
+
+    let randomId = getRandomInt(1, 10000000);
+    
+    guardarSession(randomId,supermercadoArray);
+
+    
+
+    const resultadoComparacion = document.getElementById('resultadoComparacion');
+    const resultadoComparacion1 = document.getElementById('resultadoComparacionLista');
+    resultadoComparacion.innerHTML= '<h3>Comparación de Precios</h3>';
+    supermercadoArray.forEach(s => {
+        resultadoComparacion1.innerHTML +=`<li>${s.name}: $${s.total.toFixed(2)}</li>`;
+    });
+
+    
+}
+
+
+
+crearCard();
+
+
+document. getElementById('compararButton').addEventListener('click', compararPrecios);
